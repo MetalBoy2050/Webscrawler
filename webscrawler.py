d42193a8fddd1c7912de0a06f3b1503190b21bfe
MetalBoy2050 <metalboy2050@gmail.com>
2022-05-10 21:52:35 +0300

    We made with ThreadPoolExecutor :))

--- webscrawler.py
+++ webscrawler.py
@@ -1,5 +1,4 @@
 import queue
-from threading import Thread
 import requests
 import time
 from concurrent.futures import ThreadPoolExecutor
@@ -7,7 +6,7 @@ import re
 from queue import Queue
 
 
-def openLink(url):
+def openLink(url: str) -> None:
     patternUrlDomain = r'https?://[\w_-]+(?:(?:\.[\w_-]+)+)/'
     patternHref = r'href *= *"([^"]*)"'
     urlDomain = re.search(patternUrlDomain, url).group(0)
@@ -25,7 +24,7 @@ def openLink(url):
         currText = requests.get(url).text
         currListLinks = re.findall(patternHref, currText)
 
-        for link in currListLinks:
+        def handle_link(link: str) -> None:
             linkDomain = re.search(patternUrlDomain, link)
 
             if not linkDomain:
@@ -33,16 +32,23 @@ def openLink(url):
                     response = requests.get(urlDomain + link)
                     if response.status_code == 200:
                         queueLinks.put(urlDomain + link)
-                continue
+                return
 
             linkDomain = linkDomain.group(0)
 
             if linkDomain != urlDomain or link in setLinks:
-                continue
+                return
 
             response = requests.get(link)
             if response.status_code == 200:
                 queueLinks.put(link)
+
+        t = time.perf_counter()
+        if currListLinks:
+            with ThreadPoolExecutor(max_workers=80) as pool:
+                pool.map(handle_link, currListLinks)
+
+        print(f"Time took: {time.perf_counter() - t:.2f}s")
 
 
 if __name__ == '__main__':
